61322c7a53aa9b4d628f7c2157aec2202270ce78
dachan <dachan@users.noreply.github.com>
2026-09-15 01:02:24 +0000

    Include ESP random header explicitly

--- date-time-display/main/st7735.c
+++ date-time-display/main/st7735.c
@@ -8,7 +8,7 @@
 #include "esp_check.h"
 #include "esp_heap_caps.h"
 #include "esp_log.h"
-#include "esp_system.h"
+#include "esp_random.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
